ebdabf87adb296aa541717e3aebf60abd586add1
anko9801 <daiusa3@icloud.com>
2024-12-08 15:12:50 +0900

    🎸 feat: match 改善

--- app/go/internal_handlers.go
+++ app/go/internal_handlers.go
@@ -9,9 +9,37 @@ import (
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+
+	query := `
+	WITH available_chairs AS (
+			SELECT chairs.id AS chair_id
+			FROM chairs
+			WHERE is_active = TRUE
+			AND NOT EXISTS (
+					SELECT 1
+					FROM ride_statuses rs
+					JOIN rides r ON rs.ride_id = r.id
+					WHERE r.chair_id = chairs.id
+					GROUP BY rs.ride_id
+					HAVING COUNT(rs.chair_sent_at) = 6
+			)
+			ORDER BY RAND()
+			LIMIT 1
+	)
+	SELECT rides.id AS ride_id, available_chairs.chair_id
+	FROM rides
+	CROSS JOIN available_chairs
+	WHERE rides.chair_id IS NULL
+	ORDER BY rides.created_at
+	LIMIT 1
+`
+	type Match struct {
+		RideID  int `db:"ride_id"`
+		ChairID int `db:"chair_id"`
+	}
+
+	match := &Match{}
+	if err := db.GetContext(ctx, match, query); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -20,34 +48,51 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	// マッチングされた chair_id を rides テーブルに更新
+	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", match.ChairID, match.RideID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
+
+	/* 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
+	   	ride := &Ride{}
+	   	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+	   		if errors.Is(err, sql.ErrNoRows) {
+	   			w.WriteHeader(http.StatusNoContent)
+	   			return
+	   		}
+	   		writeError(w, http.StatusInternalServerError, err)
+	   		return
+	   	}
+
+	   	matched := &Chair{}
+	   	empty := false
+	   	for i := 0; i < 10; i++ {
+	   		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
+	   			if errors.Is(err, sql.ErrNoRows) {
+	   				w.WriteHeader(http.StatusNoContent)
+	   				return
+	   			}
+	   			writeError(w, http.StatusInternalServerError, err)
+	   		}
+
+	   		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+	   			writeError(w, http.StatusInternalServerError, err)
+	   			return
+	   		}
+	   		if empty {
+	   			break
+	   		}
+	   	}
+	   	if !empty {
+	   		w.WriteHeader(http.StatusNoContent)
+	   		return
+	   	}
+
+	   	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	   		writeError(w, http.StatusInternalServerError, err)
+	   		return
+	   	} */
 
 	w.WriteHeader(http.StatusNoContent)
 }
--- app/go/main.go
+++ app/go/main.go
@@ -25,7 +25,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-var db *sqlx.DB
+var (
+	db     *sqlx.DB
+	db_sub *sqlx.DB
+)
 
 func main() {
 	mux := setup()
@@ -68,18 +71,29 @@ func setup() http.Handler {
 	dbConfig.ParseTime = true
 	dbConfig.InterpolateParams = true
 
+<<<<<<< Updated upstream
 	_db, err := sqlx.Connect("mysql", dbConfig.FormatDSN())
+=======
+	dbConfig.Addr = "192.168.0.12:3306"
+	_db1, err := sqlx.Connect("mysql", dbConfig.FormatDSN())
+	dbConfig.Addr = "192.168.0.13:3306"
+	_db2, err := sqlx.Connect("mysql", dbConfig.FormatDSN())
+>>>>>>> Stashed changes
 	if err != nil {
 		panic(err)
 	}
-	db = _db
+	db = _db1
+	db_sub = _db2
 
 	// プール内に保持できるアイドル接続数の制限を設定 (default: 2)
 	db.SetMaxIdleConns(1024)
+	db_sub.SetMaxIdleConns(1024)
 	// 接続してから再利用できる最大期間
 	db.SetConnMaxLifetime(0)
+	db_sub.SetConnMaxLifetime(0)
 	// アイドル接続してから再利用できる最大期間
 	db.SetConnMaxIdleTime(0)
+	db_sub.SetConnMaxIdleTime(0)
 
 	http.DefaultTransport.(*http.Transport).MaxIdleConns = 0           // default: 100
 	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 1024 // default: 2
